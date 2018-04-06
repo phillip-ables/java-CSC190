778f35d3d0f49f300394ddfd711464b0bd4dab46
lip ables <phillipables1@gmail.com>
2018-04-06 13:19:35 -0400

    main comment

--- Lab11/src/lab11/Lab11.java
+++ Lab11/src/lab11/Lab11.java
@@ -4,11 +4,17 @@
  * and open the template in the editor.
  */
 package lab11;
-
-/**
- *
- * @author phill
- */
+/*
+ course: CSC190
+ project: lab11
+ date:4-6
+ author: Phillip Ables
+ purpose: 
+The computer randomly generates a secret integer number in range [0, 999]. 
+Then the user keeps guessing the secret number until the correct one is entered, or the user has tried 10 times. 
+If the user guesses the correct number within 10 times, the program displays “Congratulations, you won the game! You got it in ??? tries”, and then exits. Otherwise, the program displays “Sorry, you lost the game! The answer should be ???”. 
+Every time when the user enters a wrong number, the program should give the user a clue whether the entered number is too small or too large.
+*/
 public class Lab11 {
 
     /**
