33c37e18858f9bdb7ff76209a9b6c21ed30ec8a7
lip ables <phillipables1@gmail.com>
2018-04-13 00:24:44 -0400

    finished mostly, couldnt get it to take input before it exited loop

--- Hw10/src/hw10/Hw10.java
+++ Hw10/src/hw10/Hw10.java
@@ -1,5 +1,6 @@
 package hw10;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -23,7 +24,71 @@ taking a least one but at most half of the marbles.
  */
 public class Hw10 {
     public static void play(int max){
-        
+        Boolean keepPlaying = true;
+        String play = "";
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+        int upper, lower, size, turn, two, take;
+        String power2 = "";
+        upper = max;
+        lower = 10;
+        //if odd then computers turn
+        turn = 0;
+        take = 0;
+        //random value from upper bounds or guess to lower bounds of 10
+        size = rnd.nextInt(upper-lower)+lower;
+        size = 100;
+        //choosing randomly who goes first
+        if(rnd.nextBoolean()){
+            turn++;
+        }
+        do{
+            if(size > 0){
+                System.out.println("size is : "+size);
+                //computers turn
+                if(turn%2 == 0){
+                    two = 2;
+                    while(two*2 <= size){
+                        two *= 2;
+                    }
+                    if(size == 1){
+                        size = 0;
+                    }
+                    else if(size == (two-1)){//if size is one less then power of two
+                        take = rnd.nextInt((two-1)-1)+1;  //random number of one less then power of two and one
+                    }else{
+                        size = two -1;
+                        take = 0;
+                    }
+                    System.out.println("Computers turn, marbles minus "+size);
+                }else{//players turn
+                    System.out.println("Players turn : ");
+                    take = in.nextInt();
+                }
+                size -= take;
+                turn++;
+            }else{
+                //turn now equals whos turn it would be meaning whos turn it was last is who lost
+                if(turn%2 != 0){
+                    System.out.println("COMPUTER TOOK LAST MARBLE");
+                }else{
+                    System.out.println("PLAYER TOOK THE LAST MARBLE");
+                }
+                
+                
+                
+                //could not get this last line working 
+                //origianlly just had do while charAt 0 equal y
+                //in no exploration has it not ended before allowing me to give an input
+
+                System.out.println("Do you wish to play again?");
+                play = in.nextLine().toLowerCase();
+                if(play.indexOf("y") != 0){
+                    keepPlaying = false;
+                }
+            }
+
+        }while(keepPlaying);
     }
     
     
